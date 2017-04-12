9b93606a2ac7a189fff3126561b26da505e01e43
zritchey <zachrritchey@gmail.com>
2017-04-12 10:32:17 -0400

    hi

--- src/Student.java
+++ src/Student.java
@@ -2,8 +2,16 @@
  * Created by zr162 on 4/12/17.
  */
 public class Student {
-    public Student(){
 
+    private final int number;
+    public Student(int n){
+        number=n;
+    }
+    public void add (int score){//    /    //?
+
+    }
+    public int search(int student){
+        ////return index of match
     }
 
 }
